997bd5d285fab28fcbdca52cb9479d50a9c431dc
Arthur Carlini <arthurhabowski@outlook.com>
2023-01-29 21:36:02 -0300

    adicionar componente de tabela de produtos

---
+++ src/components/ProductsTable/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = ({id, name, price, totalAmount, date}) => {
+    return (
+        <div id='table'>
+            <p>table</p>
+        </div>
+    )
+}
+
+export default index
