9d139cd6b6fbd982645d9c58f41c69d3bfc01220
Arthur <arthurhabowski@outlook.com>
2023-02-05 15:45:02 -0300

    move some functions to Products component

--- src/components/SearchProducts/index.jsx
+++ src/components/SearchProducts/index.jsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-
-const index = ({ products }) => {
-
-    const [search, setSearch] = useState('')
-    const results = products.filter(products => products.productName.toLowerCase().indexOf(search) !== -1)
+const index = ({ searchProducts, setSearchedProduct }) => {
 
     function handleInput(e) {
-        setSearch(e.target.value.toLowerCase())
+        setSearchedProduct(e.target.value.toLowerCase())
     }
 
     return (
         <div>
             <input
-                type="search"
+                type="input"
                 placeholder='Procurar...'
-                value={search}
+                value={searchProducts}
                 onChange={handleInput} />
         </div>
     )
